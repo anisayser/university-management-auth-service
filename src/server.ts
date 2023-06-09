a155ceaa5f8019e74c5faa4a01af9432ec0358b3
Anis Ayser <anisayser1@gmail.com>
2023-06-09 13:58:39 +0600

    User interface & Model added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // getting-started.js
 import mongoose from 'mongoose'
 import app from './app'
-import config from '../config/index'
+import config from './config/index'
 
 async function bootstrap() {
   try {
